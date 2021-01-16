749f284ade51e3facbeeb7ed39831e1e6cf1a82e
Alexander Petrovski <apfb11@gmail.com>
2021-01-16 17:54:49 -0500

    all in one file

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -32,6 +32,22 @@ export default class SearchField extends Component {
         console.log(this.state.gifArr)
     }
 
+    update(dataPlus) {
+        this.setState({ data: dataPlus });
+    }
+
+    componentDidMount() {
+        fetch(`https://api.giphy.com/v1/gifs/trending?api_key=iCou32qYmtidVoNOAQl5QJtlpLVRNfzk`)
+            .then((output) => output.json())
+            .then((output) => { this.setState({ gifArr: output.gifArr }) })
+
+        .catch((err) => console.error(err))
+    }
+
+    update(dataPlus) {
+        this.setState({ gifArr: dataPlus });
+    }
+
     render () {
         return (
         <div>
@@ -47,3 +63,18 @@ export default class SearchField extends Component {
         )
     }
 }
+
+/*
+    render() {
+        return (
+            <div>
+                <div>
+                    <SearchField update={this.update} />
+                </div> 
+                <div>{this.state.data.map((val, index) => (
+                    <GifCard key={index} url={val.images.original.url}/>))}         
+                </div>
+            </div>
+        )
+    }
+*/
